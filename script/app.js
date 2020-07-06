204e17e5496dde31b6c866d100cd20cea5535475
len4ik20 <len4ik_k@inbox.lv>
2020-07-06 18:57:42 +0300

    imp: home page ssheets

---
+++ script/app.js
@@ -0,0 +1,14 @@
+
+let presidentOfTheUnitedStates = 'Donald';
+let age = 18;
+let isAdmin = true;
+
+let isNull = null;
+let isUndefined = undefined;
+
+const PI = 3.14;
+
+console.log( presidentOfTheUnitedStates );
+console.error( presidentOfTheUnitedStates );
+
+
